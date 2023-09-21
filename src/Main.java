ff4fef2d71f8b8822e6274ae1739d8428a81f934
angusssyyy <angusw2026@student.cis.edu.hk>
2023-09-21 09:05:51 +0800

    unit 2 quiz

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+/* I have neither given nor received any unauthorised aid on this piece of work*/
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int resultOne = add(1, 2);
+        System.out.println(resultOne);
+
+        String resultTwo = greeting("Audrey");
+        System.out.println(resultTwo);
+
+        int resultThree = add(5, 6, 7, 8);
+        System.out.println(resultThree);
+
+        printMe("Oh no this test is so scaryyy");
     }
 
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public static String greeting(String name) {
+        return "Bonjour, " + name + "!";
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        return add(add(add(numOne, numTwo), numThree), numFour);
+    }
+
+    public static void printMe(String toBePrinted) {
+        System.out.println(toBePrinted);
+    }
 }
